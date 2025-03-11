50df641de83d2c6852580873ed5506fdd32e2212
camille.minaudo <camille.minaudo@ub.edu>
2025-03-11 15:12:41 +0100

    a generic function to compute density of dC_dt

---
+++ R/get_dCdt_density.R
@@ -0,0 +1,19 @@
+
+get_dCdt_density <- function(dataframe){
+  mydf <- data.frame(POSIX.time = dataframe$POSIX.time,
+                     time = as.numeric(dataframe$POSIX.time-first(dataframe$POSIX.time)),
+                     conc = dataframe[[gastype]])
+
+  mydf <- mydf[!duplicated(mydf$time),]
+
+  # smooth signal
+  concsmooth <- smooth.spline(x = mydf$time, y = mydf$conc, nknots = round(length(mydf$time)/3), spar = 0.8)
+  mydf$concsmooth <- approx(concsmooth$x, concsmooth$y, xout = mydf$time, rule = 2)$y
+
+  # computing first derivative
+  mydf$dydt <- get_dxdy(mydf$time, mydf$concsmooth)
+
+  # computing density probability of first derivative
+  d <- density(mydf$dydt)
+  return(list(d, mydf))
+}
