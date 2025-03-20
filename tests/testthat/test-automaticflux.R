8bf2ea8bc3fd6970db9ab05b767e17cb3a8ec8a8
camille.minaudo <camille.minaudo@ub.edu>
2025-03-20 12:53:05 +0100

    new tests

--- tests/testthat/test-automaticflux.R
+++ tests/testthat/test-automaticflux.R
@@ -1,13 +1,86 @@
 test_that("plot overview works", {
 
   library(aquaGHG)
+  library(lubridate)
+  library(tidyr)
+  library(dplyr)
+  library(pbapply)
+  library(ggplot2)
+  library(egg)
+
   # plot incubations overview
-  p <- plot.incubations(dataframe = mydata_all)
+  p <- plot_incubations(dataframe = mydata_all)
   print(p)
+})
+
+
+
+test_that("automatic CO2 calculation works with method 'trust.it.all' ", {
+
+  library(aquaGHG)
+  library(lubridate)
+  library(tidyr)
+  library(dplyr)
+  library(pbapply)
+  library(ggplot2)
+  library(egg)
+  library(goFlux)
+  library(purrr)
+
 
   # automatic inspection of CO2 data and flux calculation
-  CO2_flux <- automaticflux(dataframe = mydata_all, myauxfile = myauxfile, shoulder = 0, gastype = "CO2dry_ppm",
-                            fluxSeparation = F, displayPlots = T,
-                            method = "trust.it.all", force.separation = F) # "trust.it.all" or "focus.on.linear"
+  CO2_flux <- automaticflux(dataframe = mydata_all, myauxfile = myauxfile, shoulder = 30, gastype = "CO2dry_ppm",
+                            fluxSeparation = FALSE, displayPlots = TRUE,
+                            method = "trust.it.all", force.separation = FALSE) # "trust.it.all" or "focus.on.linear"
 
 })
+
+
+
+
+test_that("automatic CO2 calculation works with method 'trust.it.all' ", {
+
+  library(aquaGHG)
+  library(lubridate)
+  library(tidyr)
+  library(dplyr)
+  library(pbapply)
+  library(ggplot2)
+  library(egg)
+  library(goFlux)
+  library(purrr)
+
+
+  # automatic inspection of CO2 data and flux calculation
+  CO2_flux <- automaticflux(dataframe = mydata_all, myauxfile = myauxfile, shoulder = 30, gastype = "CO2dry_ppm",
+                            fluxSeparation = FALSE, displayPlots = TRUE,
+                            method = "focus.on.linear", force.separation = FALSE) # "trust.it.all" or "focus.on.linear"
+
+})
+
+
+
+
+
+test_that("automatic CH4 calculation works with method 'trust.it.all' and flux separation ", {
+
+  library(aquaGHG)
+  library(lubridate)
+  library(tidyr)
+  library(dplyr)
+  library(pbapply)
+  library(ggplot2)
+  library(egg)
+  library(goFlux)
+  library(purrr)
+  library(ggnewscale)
+
+
+  # automatic inspection of CO2 data and flux calculation
+  CH4_flux <- automaticflux(dataframe = mydata_all, myauxfile = myauxfile, shoulder = 30, gastype = "CH4dry_ppb",
+                            fluxSeparation = T, displayPlots = TRUE,
+                            method = "trust.it.all", force.separation = FALSE) # "trust.it.all" or "focus.on.linear"
+
+})
+
+
